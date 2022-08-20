b3dc65ae1fc2eb327501e75808ae2d43992e04f7
Henrique Brandão <hb06ars@hotmail.com>
2022-08-20 14:29:21 -0300

    Servico para atualizar chamado implementado.

--- src/app/components/chamado/chamado-update/chamado-update.component.ts
+++ src/app/components/chamado/chamado-update/chamado-update.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Chamado } from 'src/app/models/chamado';
 import { Cliente } from 'src/app/models/cliente';
@@ -42,10 +42,13 @@ export class ChamadoUpdateComponent implements OnInit {
     private clienteService: ClienteService,
     private tecnicoService: TecnicoService,
     private toastService: ToastrService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
+    this.chamado.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
     this.findAllClientes();
     this.findAllTecnicos();
   }
@@ -59,10 +62,27 @@ export class ChamadoUpdateComponent implements OnInit {
     })
   }
 
+  findById() : void{
+    this.chamadoService.findById(this.chamado.id).subscribe(resposta =>{
+      this.chamado = resposta;
+    }, ex =>{
+      this.toastService.error(ex.error.error)
+    })
+  }
+
+  update(): void{
+    this.chamadoService.create(this.chamado).subscribe(resposta =>{
+      this.toastService.success('Chamado atualizado com sucesso!', 'Atualizar chamado');
+      this.router.navigate(['chamados'])
+    }, ex =>{
+      this.toastService.error(ex.error.error)
+    })
+  }
+
   findAllTecnicos(): void{
     this.tecnicoService.findAll().subscribe(resposta =>{
       this.tecnicos = resposta;
-    })
+    }) 
   }
 
   findAllClientes(): void{
@@ -74,6 +94,26 @@ export class ChamadoUpdateComponent implements OnInit {
   validaCampos(): boolean {
     return this.prioridade.valid && this.status.valid && this.titulo.valid 
        && this.observacoes.valid && this.tecnico.valid && this.cliente.valid
+  }
+
+  retornaPrioridade(prioridade: any): string{
+    if(prioridade == '0'){
+      return 'BAIXA'
+    } else if(prioridade == '1') {
+      return 'MÉDIA'
+    } else{
+      return 'ALTA'
+    }
+  }
+
+  retornaStatus(status: any): string{
+    if(status == '0'){
+      return 'ABERTO'
+    } else if(status == '1'){
+      return 'EM ANDAMENTO'
+    } else{
+      return 'ENCERRADO'
+    }
   }
 
 }
